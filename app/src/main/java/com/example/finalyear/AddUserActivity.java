2872538a21d2569d4984bf695373d5960cfb3bd5
habib2013 <nairabet25@gmail.com>
2019-10-02 13:26:58 +0100

    new commit

--- app/src/main/java/com/example/finalyear/AddUserActivity.java
+++ app/src/main/java/com/example/finalyear/AddUserActivity.java
@@ -1,12 +1,19 @@
 package com.example.finalyear;
 
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.getkeepsafe.taptargetview.TapTarget;
+import com.getkeepsafe.taptargetview.TapTargetSequence;
+
+import es.dmoral.toasty.Toasty;
 
 
 public class AddUserActivity extends AppCompatActivity {
@@ -25,5 +32,44 @@ back.setOnClickListener(new View.OnClickListener() {
     }
 });
 }
+
+    private void tapview() {
+
+        new TapTargetSequence(this)
+                .targets(
+
+                        TapTarget.forView(findViewById(R.id.back), "Back Button", "Safely use the back button when you are through !")
+                                .targetCircleColor(R.color.colorAccent)
+                                .titleTextColor(R.color.colorAccent)
+                                .titleTextSize(22)
+                                .descriptionTextSize(15)
+                                .descriptionTextColor(R.color.accent)
+                                .drawShadow(true)
+                                .cancelable(false)// Whether tapping outside the outer circle dismisses the view
+                                .tintTarget(true)
+                                .transparentTarget(true)
+                                .outerCircleColor(R.color.fourth))
+                .listener(new TapTargetSequence.Listener() {
+                    // This listener will tell us when interesting(tm) events happen in regards
+                    // to the sequence
+                    @Override
+                    public void onSequenceFinish() {
+                        // session.setFirstTime(false);
+                        Toasty.success(AddUserActivity.this, " You are ready to go !", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onSequenceStep(TapTarget lastTarget, boolean targetClicked) {
+
+                    }
+
+                    @Override
+                    public void onSequenceCanceled(TapTarget lastTarget) {
+                        // Boo
+                    }
+                }).start();
+
+
+    }
 
 }
